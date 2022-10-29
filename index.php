6504fd1f6a7d4df817700c297fd8b1890a5287e6
Karen Pigni <64334155+Karen-Pigni@users.noreply.github.com>
2022-10-29 03:15:24 -0300

    CV actualizado

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="shortcut icon" href="img/KarenPigni-Logo.png" />
 
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet"/>
 
